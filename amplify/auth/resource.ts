9d90d8fcfb2802fb6cb5c37c076ca0ed59f087ea
Kiri Hood <143568635+Kiri-22@users.noreply.github.com>
2026-04-15 21:51:42 -0500

    Add postConfirmation function definition

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,5 +1,10 @@
 import { defineAuth } from '@aws-amplify/backend';
 
+import { defineFunction } from '@aws-amplify/backend';
+
+export const postConfirmation = defineFunction({
+  name: 'post-confirmation',
+});
 /**
  * Define and configure your auth resource
  * @see https://docs.amplify.aws/gen2/build-a-backend/auth
