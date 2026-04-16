4c64823cf658c3460c2a9111b73b77d00f06ac4c
Kiri Hood <143568635+Kiri-22@users.noreply.github.com>
2026-04-15 21:55:31 -0500

    Add UserProfile schema with authorization rules

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -23,6 +23,32 @@ export const data = defineData({
   },
 });
 
+import { type ClientSchema, a, defineData } from "@aws-amplify/
+backend";
+import { postConfirmation } from "../auth/post-confirmation/resource";
+const schema = a
+ .schema({
+ UserProfile: a
+ .model({
+ email: a.string(),
+ profileOwner: a.string(),
+ })
+ .authorization((allow) => [
+ allow.ownerDefinedIn("profileOwner"),
+ ]),
+ })
+ .authorization((allow) => [allow.resource(postConfirmation)]);
+export type Schema = ClientSchema<typeof schema>;
+export const data = defineData({
+ schema,
+ authorizationModes: {
+ defaultAuthorizationMode: "apiKey",
+ apiKeyAuthorizationMode: {
+ expiresInDays: 30,
+ },
+ },
+});
+
 /*== STEP 2 ===============================================================
 Go to your frontend source code. From your client-side code, generate a
 Data client to make CRUDL requests to your table. (THIS SNIPPET WILL ONLY
